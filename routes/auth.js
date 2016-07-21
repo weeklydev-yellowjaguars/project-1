78d4709bafa4c3455f2613b22fb64db97dbaf530
Ron Rider <rrider@planitagency.com>
2016-07-21 17:28:34 -0400

    add password generation to auth reset route

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,6 @@
+var fs = require('fs');
+
+var crypto = require('crypto-js');
 var express = require('express');
 var nodemailer = require('nodemailer');
 var router = express.Router();
@@ -34,27 +37,57 @@ router.post('/login', function(req, res, next) {
 });
 
 router.post('/reset', function(req, res, next) {
-    // API key: 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
-    var transporter = nodemailer.createTransport({
-        service: 'SendGrid',
-        auth: {
-            user: 'password_reset',
-            pass: '$.Reset123'
-        }
-    });
+    // we can't request the reset without the email
+    if (!req.body || !req.body.email) {
+        return res.status(400).send(JSON.stringify({error: true, message: 'missing email'}));
+    }
 
-    // load HTML email and replace placeholders
-
-    transporter.sendMail({
-        from: 'Yellow Jaguars <weeklydev.yellowjaguars@gmail.com>',
-        to: 'rideron89@gmail.com',
-        subject: 'Password Reset',
-        text: 'You wished to reset your password?',
-        html: '<h1>Password Reset</h1>'
-    }, function(err, info) {
+    User.findOne({email: req.body.email}, function(err, user) {
         if (err) return res.send(err);
+        if (!user) return res.sendStatus(404);
 
-        res.send(JSON.stringify(info));
+        // generate new password
+        var new_password = crypto.AES.encrypt(user.username + Date.now() + user.email, 'yellowjaguars123').toString();
+        new_password = [
+            new_password[parseInt(Math.random() * (new_password.length - 1) + 1)],
+            new_password[parseInt(Math.random() * (new_password.length - 1) + 1)],
+            new_password[parseInt(Math.random() * (new_password.length - 1) + 1)],
+            new_password[parseInt(Math.random() * (new_password.length - 1) + 1)],
+            new_password[parseInt(Math.random() * (new_password.length - 1) + 1)],
+            new_password[parseInt(Math.random() * (new_password.length - 1) + 1)],
+            new_password[parseInt(Math.random() * (new_password.length - 1) + 1)],
+            new_password[parseInt(Math.random() * (new_password.length - 1) + 1)],
+            new_password[parseInt(Math.random() * (new_password.length - 1) + 1)],
+            new_password[parseInt(Math.random() * (new_password.length - 1) + 1)],
+        ].join('');
+
+        // read in the HTML email to send
+        fs.readFile('./views/reset_email.html', 'utf8', function(err, data) {
+            if (err) return res.send(err);
+
+            // replace plaeholder content in the email body with new values
+            data = data.replace('{{new_password}}', new_password);
+            data = data.replace('{{username}}', user.username);
+
+            // API key: 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
+            nodemailer.createTransport({
+                service: 'SendGrid',
+                auth: {
+                    user: 'password_reset',
+                    pass: '$.Reset123'
+                }
+            }).sendMail({
+                from: 'Yellow Jaguars <weeklydev.yellowjaguars@gmail.com>',
+                to: user.email,
+                subject: 'Password Reset',
+                text: 'Your new password at http://localhost:3000/login is ' + new_password,
+                html: data
+            }, function(err, info) {
+                if (err) return res.send(err);
+
+                res.send();
+            });
+        });
     });
 });
 
