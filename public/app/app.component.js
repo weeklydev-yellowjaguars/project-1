e5ce268fa119357af9218b7bab3f49fe522a2bf3
Ron Rider <rrider@planitagency.com>
2016-07-25 17:34:53 -0400

    fix error in previous merge

--- public/app/app.component.js
+++ public/app/app.component.js
@@ -4,7 +4,8 @@
     ng.core.Component({
         selector: 'app-main',
         template: '<app-header>Loading Header...</app-header><app-new-item></app-new-item>',
-        directives: [app.TodoItemService, app.HeaderComponent, app.NewItemComponent]
+        directives: [app.HeaderComponent, app.NewItemComponent],
+        providers: [app.TodoItemService]
     })
     .Class({
         constructor: [app.TodoItemService, function(todoItemService) {
