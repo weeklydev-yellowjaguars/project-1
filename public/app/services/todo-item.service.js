a7ae05bf400cc92ab7934d62627047441460c6a2
Ron Rider <rideron89@gmail.com>
2016-07-30 15:00:40 -0400

    add update and remove functionality to todo-item.service.js

--- public/app/services/todo-item.service.js
+++ public/app/services/todo-item.service.js
@@ -3,35 +3,30 @@
     app.TodoItemService =
     ng.core.Injectable().Class({
         constructor: function() {
-            var ajax = $.ajax({
-                url: '/todos/',
-                data: {},
+            this.todoItems = [];
 
-                // callbacks
-                success: function(data, status, xhr) {
-                    console.info(data);
-                },
-                error: function(xhr, type, error) {
-                    console.warn(error);
-                }
-            });
+            this.getTodoItems();
         },
         addTodoItem: function(item, cb) {
-            console.log(item);
+            this.todoItems.push(item);
+
+            // if logged in, save to the database
         },
         getTodoItems: function(options, cb) {
             var data = [
-                {content: 'Lorem ipsum dolor sit amet', priority: 'low', date_created: Date.now()},
-                {content: 'consectetur adipisicing elit', priority: 'low', date_created: Date.now()},
-                {content: 'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua', priority: 'low', date_created: Date.now()},
-                {content: 'Ut enim ad minim veniam', priority: 'low', date_created: Date.now()},
-                {content: 'quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat', priority: 'low', date_created: Date.now()},
-                {content: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur', priority: 'low', date_created: Date.now()},
-                {content: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum', priority: 'low', date_created: Date.now()},
-                {content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod', priority: 'low', date_created: Date.now()},
-                {content: 'consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna', priority: 'low', date_created: Date.now()},
-                {content: 'exercitation ullamco laboris nisi ut aliquip', priority: 'low', date_created: Date.now()},
+                {_id: 'a7ffag6r6adsf6g87g6fas876sd86g7f', content: 'Lorem ipsum dolor sit amet', priority: 0, date_created: Date.now()},
+                {_id: 'b7ffag6r6adsf6g87g6fas876sd86g7f', content: 'consectetur adipisicing elit', priority: 0, date_created: Date.now()},
+                {_id: 'c7ffag6r6adsf6g87g6fas876sd86g7f', content: 'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua', priority: 1, date_created: Date.now()},
+                {_id: 'd7ffag6r6adsf6g87g6fas876sd86g7f', content: 'Ut enim ad minim veniam', priority: 0, date_created: Date.now()},
+                {_id: 'e7ffag6r6adsf6g87g6fas876sd86g7f', content: 'quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat', priority: 1, date_created: Date.now()},
+                {_id: 'f7ffag6r6adsf6g87g6fas876sd86g7f', content: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur', priority: 1, date_created: Date.now()},
+                {_id: 'g7ffag6r6adsf6g87g6fas876sd86g7f', content: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum', priority: 2, date_created: Date.now()},
+                {_id: 'h7ffag6r6adsf6g87g6fas876sd86g7f', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod', priority: 0, date_created: Date.now()},
+                {_id: 'i7ffag6r6adsf6g87g6fas876sd86g7f', content: 'consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna', priority: 0, date_created: Date.now()},
+                {_id: 'j7ffag6r6adsf6g87g6fas876sd86g7f', content: 'exercitation ullamco laboris nisi ut aliquip', priority: 2, date_created: Date.now()},
             ];
+
+            this.todoItems = data;
 
             // if no callback is given, just return the data
             if (options === undefined && cb === undefined) {
@@ -45,6 +40,43 @@
             }
 
             cb(data);
+        },
+        updateItem: function(item, cb) {
+            if (typeof cb !== 'function') cb = function() {};
+
+            // send the updated item to the server
+            $.ajax({
+                data: item,
+                type: 'put',
+                url: '/todos/' + item._id,
+
+                success: function(data, status, xhr) {
+                    cb(null, item);
+                },
+                error: function(xhr, errorType, error) {
+                    console.error(error);
+
+                    cb(error);
+                }
+            });
+        },
+        removeItem: function(item, cb) {
+            if (typeof cb !== 'function') cb = function() {};
+
+            $.ajax({
+                data: item,
+                type: 'delete',
+                url: '/todos/' + item._id,
+
+                success: function(data, status, xhr) {
+                    cb(null, item);
+                },
+                error: function(xhr, errorType, error) {
+                    console.log(error);
+
+                    cb(error);
+                }
+            });
         }
     });
 
