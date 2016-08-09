188fa74acdfc327abc0ec2e84ddcb96472ad9aeb
Ron Rider <rrider@planitagency.com>
2016-08-09 13:43:29 -0400

    update some of the fields for todo items

--- models/TodoItem.js
+++ models/TodoItem.js
@@ -4,9 +4,10 @@ var Schema = mongoose.Schema;
 var todoItemSchema = new Schema({
     author_id: String,
     creation_date: {type: Date, default: Date.now},
+    updated_date: {type: Date, default: Date.now},
     title: {type: String, default: ''},
     content: {type: String, default: ''},
-    priority: {type: String, default: 'low'},
+    priority: {type: Number, default: 0},
     completed: {type: Boolean, default: false}
 });
 
